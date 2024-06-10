da8f8b550fa9a7f3c93dcb2f2c2f73b41af708c2
Jose Pulido <j.pulidomurga@gmail.com>
2024-06-10 07:56:50 -0500

    primera carga                                                                                                                                                                                                                                                                                                                                                                                                                                                   qqqqqqqqqq33333333333333333333333333333333

---
+++ src/main/java/com/crud/card/model/Card.java
@@ -0,0 +1,13 @@
+package com.crud.card.model;
+
+import lombok.Data;
+
+@Data
+public class Card {
+    int id_card;
+    String namez;
+    String numberz;
+    String typez;
+    int cvv;
+    int status;
+}
